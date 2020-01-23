d2795adfb24c1da95ec5e2ad2f738c17b9a6277e
Angel Yang <angelyang93@gmail.com>
2020-01-23 12:24:15 -0800

    update and test on Jenkins

---
+++ BankAccountProgram/src/AccountType.java
@@ -0,0 +1,19 @@
+public abstract class AccountType {
+    private int acctNum;
+    private String firstName;
+    private String lastName;
+    private String phoneNum;
+    private int SSN;
+    private double acctBalance;
+
+    public AccountType(int acctNum, String firstName, String lastName, String phoneNum, int SSN, double acctBalance) {
+        this.acctNum = acctNum;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNum = phoneNum;
+        this.SSN = SSN;
+        this.acctBalance = acctBalance;
+    }
+
+
+}
---
+++ BankAccountProgram/src/BankSystem.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class BankSystem {
+    public static void main(String[] args) {
+        AccountType accountType = new AccountType()
+    }
+}
